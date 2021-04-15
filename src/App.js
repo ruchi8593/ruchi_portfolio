1dcdff4a20cb5edfa0eaa0dc5612643cc48c3422
Ruchita Trivedi <75458159+ruchi8593@users.noreply.github.com>
2021-04-14 23:39:10 -0400

    Header complete

--- src/App.js
+++ src/App.js
@@ -1,10 +1,35 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Navbar from './components/navbar'
+import Navbar from './components/navbar';
+import Header from './components/header';
+import Particles from 'react-particles-js';
 
 function App() {
   return (
+    <>
+    <Particles
+    className="particle-canvas"
+      params={{
+        particles:{
+          number:{
+            value: 30,
+            density:{
+              enable:true,
+              value_area:900
+            }
+          },
+          shape:{
+            type:"square",
+            stroke:{
+              width:6,
+              color:"#f9ab00"
+            }
+          }
+        }
+      }}/>
     <Navbar/>
+    <Header/>
+    </>
     
   );
 }
---
+++ src/components/header.js
@@ -0,0 +1,24 @@
+import React from "react";
+import Typed from "react-typed";
+
+const header = () => {
+    return (
+        <div className="header-wraper">
+            <div className="main-info">
+                <h1>Web development and websites promotions</h1>
+                <Typed
+                    className="typed-text"
+                    strings={["Web Design", "Web Development", "Mobile Application Development"]}
+                    typeSpeed={40}
+                    backSpeed={70}
+                    loop
+                />
+                <a href="#" className="btn-main-offer">Contact Me</a>
+
+            </div>
+            
+        </div>
+    )
+}
+
+export default header
